9b4af962b96fa107cc887cfcde501693f1839c03
Keitaro <9158853+ktr17@users.noreply.github.com>
2024-02-11 10:55:05 +0900

    インデントをタブからスペースに変換

--- js/content.js
+++ js/content.js
@@ -2,49 +2,49 @@
  * クエリを受け取り、domを返却する
  */
 function getElement(domQuery) {
-	let eDom = document.querySelector(domQuery);
-	return eDom;
+  let eDom = document.querySelector(domQuery);
+  return eDom;
 }
 
 /**
  * 書籍タイトルを取得する
  */
 function getBookTitle() {
-	let bookTitle = "";
-	let bookTitleQuery = "#annotation-scroller > div > div.a-row.a-spacing-base > div.a-column.a-span5 > h3";
-	let eBookTitle = getElement(bookTitleQuery);
-	if (eBookTitle != null) {
-		bookTitle = eBookTitle.innerText;
-	}
-	return bookTitle;
+  let bookTitle = "";
+  let bookTitleQuery = "#annotation-scroller > div > div.a-row.a-spacing-base > div.a-column.a-span5 > h3";
+  let eBookTitle = getElement(bookTitleQuery);
+  if (eBookTitle != null) {
+    bookTitle = eBookTitle.innerText;
+  }
+  return bookTitle;
 }
 
 /**
  * 書籍のメモを取得する
  */
 function getBookMemo() {
-	// メモの内容を取得
-	let eBookMemos = document.getElementById("kp-notebook-annotations").children;
+  // メモの内容を取得
+  let eBookMemos = document.getElementById("kp-notebook-annotations").children;
 
-	// メモを取得して、マークダウンに変換する処理
-	let retMemoText = "";
-	for (var i = 0; i < eBookMemos.length; i++){
-		bookMemo = eBookMemos[i].getElementsByClassName("a-size-base-plus a-color-base");
-		memoPosition = eBookMemos[i].getElementsByClassName("a-size-small a-color-secondary kp-notebook-selectable kp-notebook-metadata");
-		let firstLine = ""
-		if (bookMemo.length){
-			// メモ箇所を取得
-			if (bookMemo.note.innerText != "") {
-				// 1行目に見出し2を設定する
-  			retMemoText += "## " + bookMemo.note.innerText + "\n\n";
-			}
-			// ハイライト箇所を取得
-			if (bookMemo.highlight.innerText != "") {
-				retMemoText += "> " + bookMemo.highlight.innerText + "\n\n" + memoPosition[1].innerText + "\n\n";
-			}
-		}
-	}
-	return retMemoText;
+  // メモを取得して、マークダウンに変換する処理
+  let retMemoText = "";
+  for (var i = 0; i < eBookMemos.length; i++){
+    bookMemo = eBookMemos[i].getElementsByClassName("a-size-base-plus a-color-base");
+    memoPosition = eBookMemos[i].getElementsByClassName("a-size-small a-color-secondary kp-notebook-selectable kp-notebook-metadata");
+    let firstLine = ""
+    if (bookMemo.length){
+      // メモ箇所を取得
+      if (bookMemo.note.innerText != "") {
+        // 1行目に見出し2を設定する
+        retMemoText += "## " + bookMemo.note.innerText + "\n\n";
+      }
+      // ハイライト箇所を取得
+      if (bookMemo.highlight.innerText != "") {
+        retMemoText += "> " + bookMemo.highlight.innerText + "\n\n" + memoPosition[1].innerText + "\n\n";
+      }
+    }
+  }
+  return retMemoText;
 
 }
 
@@ -52,32 +52,32 @@ function getBookMemo() {
  * 書籍の表紙イメージのリンクを取得するf
  */
 function getBookImgLink() {
-	// 書籍の表紙イメージ
-	let eBookImgLink = document.querySelector("#annotation-scroller > div > div.a-row.a-spacing-base > div.a-column.a-span1.kp-notebook-bookcover-container > a > span > img");
-	let retBookImgLink = "";
-	if (eBookImgLink != null) {
-		retBookImgLink = eBookImgLink.src;
-	}
-	return retBookImgLink;
+  // 書籍の表紙イメージ
+  let eBookImgLink = document.querySelector("#annotation-scroller > div > div.a-row.a-spacing-base > div.a-column.a-span1.kp-notebook-bookcover-container > a > span > img");
+  let retBookImgLink = "";
+  if (eBookImgLink != null) {
+    retBookImgLink = eBookImgLink.src;
+  }
+  return retBookImgLink;
 }
 /**
  * メッセージに対する処理を登録
  */
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	// 初期化処理(拡張機能を実行時)
-	if (request.name === "popup:content:initial") {
-		let bookTitle = getBookTitle();
-		let bookMemo = getBookMemo();
-		let bookImgLink = getBookImgLink();
+  // 初期化処理(拡張機能を実行時)
+  if (request.name === "popup:content:initial") {
+    let bookTitle = getBookTitle();
+    let bookMemo = getBookMemo();
+    let bookImgLink = getBookImgLink();
 
-		let promise = chrome.runtime.sendMessage({ name: "content:popup:bookTitleAndMemo", message: {Title: bookTitle, Memo: bookMemo, BookImgLink: bookImgLink}});
-		promise.then((response) => {
-			// 成功
-		})
-		.catch((error) => {
-			// 失敗
-			console.error(error);
-		});
-	}
+    let promise = chrome.runtime.sendMessage({ name: "content:popup:bookTitleAndMemo", message: {Title: bookTitle, Memo: bookMemo, BookImgLink: bookImgLink}});
+    promise.then((response) => {
+      // 成功
+    })
+    .catch((error) => {
+      // 失敗
+      console.error(error);
+    });
+  }
 });
 
--- js/popup.js
+++ js/popup.js
@@ -4,106 +4,106 @@
  * content.jsへKindleのメモ取得依頼メッセージを送信する
  */
 chrome.tabs.query({},function(tabs){
-	// メモの取得依頼メッセージ送信
-	chrome.tabs.query({ active: true, currentWindow: true}, initialMessage);
+  // メモの取得依頼メッセージ送信
+  chrome.tabs.query({ active: true, currentWindow: true}, initialMessage);
 });
 
 // 書籍タイトルのコピーイベントを登録
 let bookTitleCopyBtn = document.getElementById("bookTitleCopyBtn");
 bookTitleCopyBtn.addEventListener("click", () => {
-	let eBookTitle = document.getElementById("bookTitle");
-	let bookTitle = "";
-	if (eBookTitle != null) {
-		bookTitle = eBookTitle.value;
-	}
-	copyClipboad(bookTitle, bookTitleCopyBtn);
+  let eBookTitle = document.getElementById("bookTitle");
+  let bookTitle = "";
+  if (eBookTitle != null) {
+    bookTitle = eBookTitle.value;
+  }
+  copyClipboad(bookTitle, bookTitleCopyBtn);
 }, false);
 
 // 書籍の表紙リンクをコピーするイベント登録
 let bookImgLinkCopyBtn = document.getElementById("bookImgLinkCopyBtn");
 bookImgLinkCopyBtn.addEventListener("click", () => {
-	let eBookImgLink = document.getElementById("bookImgLink");
-	let bookImgLink = "";
-	if (eBookImgLink != null) {
-		bookImgLink = eBookImgLink.value;
-	}
-	copyClipboad(bookImgLink, bookImgLinkCopyBtn);
+  let eBookImgLink = document.getElementById("bookImgLink");
+  let bookImgLink = "";
+  if (eBookImgLink != null) {
+    bookImgLink = eBookImgLink.value;
+  }
+  copyClipboad(bookImgLink, bookImgLinkCopyBtn);
 });
 
 // 書籍のメモをコピーするイベント登録
 let bookMemoCopyBtn = document.getElementById("bookMemoCopyBtn");
 bookMemoCopyBtn.addEventListener("click", () => {
-	let eBookMemo = document.getElementById("bookMemo");
-	let bookMemo = "";
-	if (eBookMemo != null) {
-		bookMemo = eBookMemo.value;
-	}
-	copyClipboad(bookMemo, bookMemoCopyBtn);
+  let eBookMemo = document.getElementById("bookMemo");
+  let bookMemo = "";
+  if (eBookMemo != null) {
+    bookMemo = eBookMemo.value;
+  }
+  copyClipboad(bookMemo, bookMemoCopyBtn);
 });
 
 
 // Content.jsから受け取った書籍タイトルと書籍メモをテキストエリアに表示する処理を登録
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	// 拡張機能をクリック時に取得したbookTitleをテキストエリア(ID: bookTitle)に表示する
-	if (request.name === "content:popup:bookTitleAndMemo") {
-		// 書籍タイトルが空欄のときは正常に取得できなかったため、エラー処理を行う
-		if (request.message.Title == "") {
-			alert("エラー");
-			let eMemoArea = document.getElementById("memoArea");
-			eMemoArea.style.display = 'none';
-			return;
-		}
-		// 書籍タイトルの表示
-		let eTitle = document.getElementById("bookTitle");
-		if (eTitle != null) {
-			if (request.message.Title == "") {
-				eTitle.value = "取得できませんでした。";
-			} else {
-				eTitle.value = request.message.Title;
-			}
-		}
-		// 表紙リンクの表示
-		let eBookImgLink = document.getElementById("bookImgLink");
-		if (eBookImgLink != null) {
-			if (request.message.BookImgLink == "") {
-				eBookImgLink.value = "取得できませんでした。";
-			} else {
-				eBookImgLink.value = request.message.BookImgLink;
-			}
-		}
-		// メモの表示
-		let eMemo = document.getElementById("bookMemo");
-		if (eMemo != null) {
-			if (request.message.Memo == "") {
-				eMemo.value = "取得できませんでした。";
-			} else {
-				eMemo.value = request.message.Memo;
-			}
-		}
-	}
+  // 拡張機能をクリック時に取得したbookTitleをテキストエリア(ID: bookTitle)に表示する
+  if (request.name === "content:popup:bookTitleAndMemo") {
+    // 書籍タイトルが空欄のときは正常に取得できなかったため、エラー処理を行う
+    if (request.message.Title == "") {
+      alert("エラー");
+      let eMemoArea = document.getElementById("memoArea");
+      eMemoArea.style.display = 'none';
+      return;
+    }
+    // 書籍タイトルの表示
+    let eTitle = document.getElementById("bookTitle");
+    if (eTitle != null) {
+      if (request.message.Title == "") {
+        eTitle.value = "取得できませんでした。";
+      } else {
+        eTitle.value = request.message.Title;
+      }
+    }
+    // 表紙リンクの表示
+    let eBookImgLink = document.getElementById("bookImgLink");
+    if (eBookImgLink != null) {
+      if (request.message.BookImgLink == "") {
+        eBookImgLink.value = "取得できませんでした。";
+      } else {
+        eBookImgLink.value = request.message.BookImgLink;
+      }
+    }
+    // メモの表示
+    let eMemo = document.getElementById("bookMemo");
+    if (eMemo != null) {
+      if (request.message.Memo == "") {
+        eMemo.value = "取得できませんでした。";
+      } else {
+        eMemo.value = request.message.Memo;
+      }
+    }
+  }
 });
 
 /**
  * 現在開いているタブにデータを送信する
  */
 function initialMessage(tabs) {
-	// 現在開いているタブにメッセージを送る(send_data, callbackは省略可能)
-	let promise = chrome.tabs.sendMessage(tabs[0].id, { name: "popup:content:initial"});
-	promise.then((response) => {
-		// 成功
-		let eMemoArea = document.getElementById("memoArea");
-		eMemoArea.style.display = 'block';
-		let eErrorMessage = document.getElementById("errorMessage");
-		eErrorMessage.style.display = 'none';
+  // 現在開いているタブにメッセージを送る(send_data, callbackは省略可能)
+  let promise = chrome.tabs.sendMessage(tabs[0].id, { name: "popup:content:initial"});
+  promise.then((response) => {
+    // 成功
+    let eMemoArea = document.getElementById("memoArea");
+    eMemoArea.style.display = 'block';
+    let eErrorMessage = document.getElementById("errorMessage");
+    eErrorMessage.style.display = 'none';
 
-	})
-	.catch((error) => {
-		// 失敗
-		let eMemoArea = document.getElementById("memoArea");
-		eMemoArea.style.display = 'none';
-		let eErrorMessage = document.getElementById("errorMessage");
-		eErrorMessage.style.display = 'block';
-	});
+  })
+  .catch((error) => {
+    // 失敗
+    let eMemoArea = document.getElementById("memoArea");
+    eMemoArea.style.display = 'none';
+    let eErrorMessage = document.getElementById("errorMessage");
+    eErrorMessage.style.display = 'block';
+  });
 }
 
 /**
@@ -111,18 +111,18 @@ function initialMessage(tabs) {
  * @param {copyText} コピー対象の文字列
  */
 function copyClipboad(copyText, eDom) {
-	// クリップボードにコピーできるかをチェック
-	if(!navigator.clipboard) {
-		alert("コピーできません。");
-	}
+  // クリップボードにコピーできるかをチェック
+  if(!navigator.clipboard) {
+    alert("コピーできません。");
+  }
 
-	// コピー実行
-	let promise = navigator.clipboard.writeText(copyText);
-	promise.then((response) => {
-		eDom.innerHTML = "Copied!"
-		// 1秒後にボタンの文字列を「Copy」に変更する。
-		setTimeout(() => (eDom.innerHTML = 'Copy'), 1000);
-	}).catch((error) => {
-		alert("コピーに失敗しました。");
-	});
+  // コピー実行
+  let promise = navigator.clipboard.writeText(copyText);
+  promise.then((response) => {
+    eDom.innerHTML = "Copied!"
+    // 1秒後にボタンの文字列を「Copy」に変更する。
+    setTimeout(() => (eDom.innerHTML = 'Copy'), 1000);
+  }).catch((error) => {
+    alert("コピーに失敗しました。");
+  });
 }
